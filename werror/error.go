7b4058ef05c65b345caa6586188a09f328f0e05f
WingGao <wing.gao@live.com>
2025-02-08 04:12:00 +0800

    feat: error

--- werror/error.go
+++ werror/error.go
@@ -4,11 +4,16 @@ import "github.com/gookit/goutil/errorx"
 
 type BizError struct {
 	errorx.ErrorX
+	Code int
 }
 
-func NewBizError(msg string) *BizError {
+func NewBizError(msg string, code ...int) *BizError {
 	ex := errorx.New(msg).(*errorx.ErrorX)
-	return &BizError{*ex}
+	c := ERROR_CODE_NORMAL
+	if len(code) > 0 {
+		c = code[0]
+	}
+	return &BizError{*ex, c}
 }
 
 func PanicError(errs ...error) {
---
+++ werror/weberror.go
@@ -0,0 +1,6 @@
+package werror
+
+const (
+	ERROR_CODE_NOT_LOGIN = 101   // 未登录
+	ERROR_CODE_NORMAL    = 10000 // 普通
+)
